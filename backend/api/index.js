c2084c9ecf8e3dff88631c19aedb8cfdee6fe7ec
Agustin-Islas <agustinislas44@hotmail.com>
2025-08-06 18:17:59 -0300

    prueba3

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,14 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/registros', (req, res) => {
-  res.json({ mensaje: 'registros ok desde express' });
+  res.json({ mensaje: '¡Express y rutas funcionando en Vercel!' });
 });
 
 module.exports = serverless(app);
